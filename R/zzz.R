985ec801a1e66c85d2d7bf41792b4e865d0cf9de
Gregor <gregor@cosyne.com>
2026-02-09 09:38:22 +0100

    feat: updated makevars

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onLoad <- function(...) {
+  # S7
   S7::methods_register()
+
+  # Create torch cache directories
+  cache_dir <- file.path(tempdir(), ".torch")
+  dir.create(cache_dir, showWarnings = FALSE, recursive = TRUE)
+  Sys.setenv(TORCH_HOME = cache_dir)
 }
